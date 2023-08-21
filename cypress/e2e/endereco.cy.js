82aa3bc24472383d157412e836266731f1261ef4
MmsGrillo <mmgrillo@hotmail.com>
2023-08-21 09:16:44 -0300

    inclusão massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import EnderecoPage from "../support/page-objects/endereco.page"
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Endereços de faturamento e entrega', () => {
 
@@ -15,8 +16,36 @@ describe('Endereços de faturamento e entrega', () => {
     it('Deve fazer o cadastro do endereço de faturamento com sucesso!', () => {
         EnderecoPage.editarEnderecoFaturamento('Bicho', 'da Goiaba', 'EBAC', 'brasil', 'Av. Brasil', '222', 'São Paulo', 'São paulo', '01001001', '11987654321', 'aluno_ebac@teste.com')
     });
+    it('Deve fazer o cadastro do endereço de faturamento com sucesso! - Massa de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numeroRua,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+    });
 
     it('Deve fazer o cadastro do endereço de entrega com sucesso', () => {
         EnderecoPage.editarEnderecoEntrega('Bicho', 'da Goiaba', 'EBAC', 'brasil', 'Av. Paulista', '1010', 'São Paulo', 'sao paulo', '01010101')
+    });
+
+    it.only('Deve fazer o cadastro do endereço de entrega com sucesso - Massa de dados', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].endereco,
+            dadosEndereco[0].numeroRua,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].cep
+        )
     });
 });
